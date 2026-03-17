bfe2ab9ef6c101426fae7d415f9a82054198031a
Divyanshu Singh <divyanshu.singh0504@gmail.com>
2026-03-17 09:19:42 +0530

    Add files via upload

---
+++ OOPSBannerApp UC6.java
@@ -0,0 +1,48 @@
+public class OOPSBannerUC6 {
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+    public static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
+    }
+    public static void main(String[] args) {
+
+        String[] O1 = getO();
+        String[] O2 = getO();
+        String[] P  = getP();
+        String[] S  = getS();
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(
+                    String.join("  ", O1[i], O2[i], P[i], S[i])
+            );
+        }
+    }
+}
